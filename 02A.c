bdec815191e2d746930a4533432485acd8b898ca
TakahiroI131 <rsk39141@gmail.com>
2013-10-16 15:10:49 +0900

    The Second Largest Number

--- 02A.c
+++ 02A.c
@@ -1,39 +1,25 @@
-#include <stdio.h>
+#include <stdio.h>                                                              
 #define N 10 /* 整数の個数の定義 */
 int a[N];    /* 整数を読み込む配列 */
 int main(void) {
   int i;
-  int max;     /* 配列内で最も大きな整数 */
-  int max_num; /* 最大値の数値の添字 */
-  int b[N -1]; /* 配列 a から最大値を除いた配列 */
-  for (i = 0; i < N; i ++) scanf("%d", &a[i]); /* 読み込み */
+  int max;      /* 配列内で最も大きな整数 */
+  int semi_max; /* 配列内で2番目に大きな整数 */
+  /* 読み込み */
+  for (i = 0; i < N; i ++) 
+    scanf("%d", &a[i]);
   /* 最大値を判定する */
   max = a[0];
-  for (i = 0; i < N; i ++) {
+  for (i = 1; i < N; i ++) {
     if (max <= a[i]) {
+      semi_max = max;
       max = a[i];
-      max_num = i;
-    }
+    } else if(semi_max <= a[i]) {
+      semi_max = a[i];
+    }   
   }
-  /* max を除いた配列を読み込む */
-  i = 0;
-  while (i < N - 1) {  
-    if(i == max_num) {
-      i ++;
-    }
-    else {
-      b[i] = a[i];
-      i ++;
-    }
-  }
-  /* bの最大値を判定する */
-  max = b[0];
-  for ( i = 0; i < N - 1; i ++) {
-    if (max <= b[i])
-      max = b[i];
-  }
-  
-  printf("%d\n", max); /* 出力 */
+  /* semi_max を出力する */
+  printf("%d\n", semi_max); /* 出力 */
   
   return 0;
 }
